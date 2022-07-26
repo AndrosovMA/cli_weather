9caa64c4a82c65a896d99953406a48a441287865
AndrosovMA <AndrosovMA@bk.ru>
2022-07-26 18:44:03 +0300

    create log.service.js - realisation func printError, printSuccess, printHelp

---
+++ service/log.service.js
@@ -0,0 +1,26 @@
+// install chalk - для раскрашивания log
+// install dedent-js - для удаления лишних отступов
+
+import chalk from 'chalk';
+import dedent from 'dedent-js'
+
+const printError = (error) => {
+  console.log(`${chalk.bgRed('ERROR')} ${error}`);
+};
+
+const printSuccess = (message) => {
+  console.log(` ${chalk.bgGreen('SUCCESS')} ${message}`);
+};
+
+const printHelp = () => {
+  console.log(
+      dedent`${chalk.bgCyan('HELP')}
+      Без параметров - вывод погоды
+      -s [SITY] для установки города
+      -h для вывода помощи
+      -t [API_KEY] для сохранения токена
+      `
+  );
+};
+
+export {printError, printSuccess, printHelp};
--- weather.js
+++ weather.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js'
+import {printHelp} from './service/log.service.js'
 
 // get arguments cli - process.argv
 const initCLI = () => {
   const args = getArgs(process.argv)
-  console.log(args);
 
   if (args.h) {
-    // Вывод help
+    printHelp();
   }
   if (args.s) {
     // сохранить город
